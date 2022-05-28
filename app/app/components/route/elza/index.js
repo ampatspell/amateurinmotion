bcc9ae7d72190e6cfc78e7550a9ec23b7c0fec0f
ampatspell <ampatspell@gmail.com>
2022-05-28 17:02:41 +0300

    cleanup

--- app/app/components/route/elza/index.js
+++ app/app/components/route/elza/index.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export default class RouteElzaComponent extends Component {
+export default class RouteElzaIndexComponent extends Component {
 
   @tracked isPlaying = false;
 
