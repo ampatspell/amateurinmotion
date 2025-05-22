19ec2947657c98c9891897ae06082994c0ae0482
Arnis Vuskans <ampatspell@gmail.com>
2025-05-22 22:14:02 +0300

    Update definition.svelte.ts

--- src/lib/definition.svelte.ts
+++ src/lib/definition.svelte.ts
@@ -1,5 +1,6 @@
+import { file } from '$d2/lib/definition/file/definition.svelte';
 import { app } from '$d2/lib/definition/utils.svelte';
 
 export const definition = app({
-  nodes: [],
+  nodes: [file()],
 });
