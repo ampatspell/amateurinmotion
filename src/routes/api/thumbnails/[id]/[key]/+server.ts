e4ef8a4387fc529eae3a4cbeb8bde9ca7f221d63
Arnis Vuskans <ampatspell@gmail.com>
2025-10-07 03:37:11 +0300

    temporary fix for assets proxy

--- src/routes/api/thumbnails/[id]/[key]/+server.ts
+++ src/routes/api/thumbnails/[id]/[key]/+server.ts
@@ -6,9 +6,30 @@ export const GET: RequestHandler = async ({ fetch, request, params: { id, key } 
   const token = await directus.getToken();
   const href = directus.url.href;
   const url = `${href}assets/${id}/?access_token=${token}&key=${key}`;
-  return fetch(url.toString(), {
+
+  const { body, headers, status, statusText } = await fetch(url, {
     body: request.body,
     method: request.method,
     headers: request.headers,
   });
+
+  // TODO: gzip body again
+  return new Response(body, {
+    headers: omitHeaders(headers, ['content-encoding']),
+    status,
+    statusText,
+  });
 };
+
+const omitHeaders = (headers: Headers, omit: string[]) => {
+  const ret = new Headers();
+  for(const key of headers.keys()) {
+    if(!omit.includes(key.toLowerCase())) {
+      const value = headers.get(key);
+      if(value) {
+        ret.append(key, value);
+      }
+    }
+  }
+  return ret;
+}
