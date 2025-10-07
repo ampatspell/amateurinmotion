9980df482478d44bac1ecdf997b5fe41ac6c9cf4
Arnis Vuskans <ampatspell@gmail.com>
2025-10-07 22:25:30 +0300

    Update api.svelte.ts

--- src/lib/utils/api.svelte.ts
+++ src/lib/utils/api.svelte.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_DIRECTUS_TOKEN, PUBLIC_DIRECTUS_URL } from "$env/static/public";
+import { PUBLIC_DIRECTUS_TOKEN, PUBLIC_DIRECTUS_URL } from '$env/static/public';
 
 export const resolveImageThumbnailURL = (id: string, key: string) => {
   const url = PUBLIC_DIRECTUS_URL;
