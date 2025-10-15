9130d90c09e0acc0d558ef23f7bf8e68795ddc67
Arnis Vuskans <ampatspell@gmail.com>
2025-10-16 01:26:04 +0300

    UI

--- src/lib/pages/gallery/description.svelte
+++ src/lib/pages/gallery/description.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { GalleryFileModel, GalleryModel } from '$lib/models/galleries.svelte';
+  import Like from './like.svelte';
 
   let { gallery, selected }: { gallery: GalleryModel; selected: GalleryFileModel } = $props();
 
@@ -13,27 +14,41 @@
 </script>
 
 <div class="description">
-  <div class="title">{title}</div>
-  {#if selected}
-    <div class="name">{normalize(selected.identifier)}</div>
-  {/if}
+  <div class="main">
+    <div class="title">{title}</div>
+    {#if selected}
+      <div class="name">{normalize(selected.identifier)}</div>
+    {/if}
+  </div>
+  <div class="like">
+    <Like />
+  </div>
 </div>
 
 <style lang="scss">
   .description {
+    height: 30px;
     display: flex;
     flex-direction: row;
-    align-items: baseline;
-    gap: 20px;
-    > .title {
-      font-weight: 600;
+    align-items: center;
+    gap: calc(var(--padding) / 2);
+    > .main {
+      display: flex;
+      flex-direction: row;
+      gap: calc(var(--padding) / 2);
+      align-items: baseline;
+      > .title {
+        font-weight: 600;
+      }
+      > .name {
+        font-size: 11px;
+      }
     }
-    > .name {
-      font-size: 11px;
-    }
-    @media (max-width: 768px) {
-      flex-direction: column;
-      gap: 5px;
+    > .like {
+      flex: 1;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
     }
   }
 </style>
---
+++ src/lib/pages/gallery/like.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import MynauiHeartCircleSolid from '$lib/components/icons/mynaui--heart-circle-solid.svelte';
+  import MynauiHeartCircle from '$lib/components/icons/mynaui--heart-circle.svelte';
+
+  let liked = $state(false);
+  let onclick = () => {
+    liked = !liked;
+  };
+</script>
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div class="like" {onclick}>
+  {#if liked}
+    <MynauiHeartCircleSolid />
+  {:else}
+    <MynauiHeartCircle />
+  {/if}
+</div>
+
+<style lang="scss">
+  .like {
+    width: 30px;
+    height: 30px;
+  }
+</style>
